037b356a7aa3ebd04e0e3df6644bc03922224059
Ryoichiro Kitahara <175901292+ryoichi1470@users.noreply.github.com>
2024-08-17 02:44:41 +0000

    bookのindexページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,12 @@
 class BooksController < ApplicationController
-  def new
-    @books = Book.new
-  end
   
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/top'
-  end 
-
   def index
     @books = Book.all
+    @book = Book.new
   end
 
-  def show
+  def create
+    @book = Book.find(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'top' => 'homes#top'
   resources :books
+  root to: 'home#top'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show'
 end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/rou
